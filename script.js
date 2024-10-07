11d5023ae11c5f1d60d93a38f1451deced3415dd
CELIO LAZARO PEREIRA <celio.lpereira@senacsp.edu.br>
2024-10-07 16:29:11 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,11 @@
+let roupas = ["camiseta", "blusa", "calca", "meia"];
+// console.log (roupas);
+// 
+for (let i = 0; i < roupas.length; i++) {
+    console.log(roupas[i]);
+}
+    // let roupas = roupas.map((roupas)) = {
+    //     return roupas.length;
+    //     });
+
+    //     console.log(roupas)
